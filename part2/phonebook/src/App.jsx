a8232f02075647d1c3743c0402e5e42209252c6c
Javier <javier.oterofgmail.com>
2025-12-23 19:37:06 +0100

    refactoring

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -6,6 +6,7 @@ import personService from './services/persons'
 
 const App = () => {
   const [persons, setPersons] = useState([])
+  const [searchPerson, setSearchPerson] = useState('')
 
   useEffect(() => {
     personService
@@ -15,46 +16,33 @@ const App = () => {
       })
   }, [])
 
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-  const [searchPerson, setSearchPerson] = useState('')
 
-  const addPerson = (event) => {
-    event.preventDefault()
-    const personObject = {
-      name: newName,
-      number: newNumber,
-    }
-    const existingPerson = persons.find(person => person.name === newName)
+
+  const addPerson = (personObject) => {
+    const existingPerson = persons.find(person => person.name === personObject.name)
     if (existingPerson) {
-      const confirmMessage = window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)
+      const confirmMessage = window.confirm(`${personObject.name} is already added to phonebook, replace the old number with a new one?`)
       if (confirmMessage) {
         personService
           .update(existingPerson.id, personObject)
           .then(response => {
-            setPersons(persons.map(person => 
+            setPersons(persons.map(person =>
               person.id !== existingPerson.id ? person : response.data
             ))
-            setNewName('')
-            setNewNumber('')
           })
       }
-      
     } else {
       personService
         .create(personObject)
         .then(response => {
           setPersons(persons.concat(response.data))
-          setNewName('')
-          setNewNumber('')
         })
     }
   }
 
-  const handleNameChange = (event) => setNewName(event.target.value)
-  const handleNumberChange = (event) => setNewNumber(event.target.value)
   const handleSearchChange = (event) => setSearchPerson(event.target.value)
   const personsToShow = persons.filter(person => person.name.toLowerCase().includes(searchPerson.toLowerCase()))
+
   const handleDelete = id => {
     const person = persons.find(n => n.id === id)
     if (window.confirm(`Delete ${person.name}?`)) {
@@ -70,17 +58,11 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <Filter searchPerson={searchPerson} handleSearchChange={handleSearchChange}/>
+      <Filter searchPerson={searchPerson} handleSearchChange={handleSearchChange} />
       <h2>add a new</h2>
-      <PersonForm
-        addPerson={addPerson}
-        newName={newName}
-        handleNameChange={handleNameChange}
-        newNumber={newNumber}
-        handleNumberChange={handleNumberChange}
-      />
+      <PersonForm createPerson={addPerson}/>
       <h2>Numbers</h2>
-      <Persons personsToShow={personsToShow} handleDelete={handleDelete}/>
+      <Persons personsToShow={personsToShow} handleDelete={handleDelete} />
     </div>
   )
 }
--- part2/phonebook/src/components/PersonForm.jsx
+++ part2/phonebook/src/components/PersonForm.jsx
@@ -1,11 +1,26 @@
-const PersonForm = ({ addPerson, newName, handleNameChange, newNumber, handleNumberChange }) => {
+import { useState } from "react"
+
+const PersonForm = ({ createPerson }) => {
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    createPerson({
+      name: newName,
+      number: newNumber
+    })
+    setNewName('')
+    setNewNumber('')
+  }
+
   return (
     <form onSubmit={addPerson}>
       <div>
-        name: <input value={newName} onChange={handleNameChange} />
+        name: <input value={newName} onChange={e => setNewName(e.target.value)} />
       </div>
       <div>
-        number : <input value={newNumber} onChange={handleNumberChange} />
+        number : <input value={newNumber} onChange={e => setNewNumber(e.target.value)} />
       </div>
       <div>
         <button type="submit">add</button>
