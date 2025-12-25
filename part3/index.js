b06f175d14a3dc89a84e2ef09f817b6ce799214f
Javier <javier.oterofgmail.com>
2025-12-25 20:02:21 +0100

    exercise 3.8: phonebook backend, step 8

--- part3/index.js
+++ part3/index.js
@@ -3,6 +3,7 @@ const morgan = require('morgan')
 const app = express()
 
 
+morgan.token('body', function (req, res) {return JSON.stringify(req.body)})
 app.use(express.json())
 
 let persons = [
@@ -28,7 +29,7 @@ let persons = [
   }
 ]
 
-app.use(morgan('tiny'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.get('/info', (request, response) => {
   const now = new Date().toString()
