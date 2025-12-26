ff220307b10be4d16cb804cc091499386105521a
Javier <javier.oterofgmail.com>
2025-12-26 10:35:29 +0100

    exercise 3.9: phonebook step 9

--- part3/index.js
+++ part3/index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
+app.use(cors())
 
 morgan.token('body', function (req, res) {return JSON.stringify(req.body)})
 app.use(express.json())
+app.use(express.static('dist'))
 
 let persons = [
   {
@@ -93,7 +96,7 @@ app.post('/api/persons', (request, response) => {
 
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
