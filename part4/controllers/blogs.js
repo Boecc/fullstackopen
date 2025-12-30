9367824ee59cd687fcc3b75b31dbd2fdfcfb5e96
Javier <javier.oterofgmail.com>
2025-12-30 19:06:46 +0100

    exercise 4.22 step 10

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,7 +1,5 @@
-const jwt = require('jsonwebtoken')
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
-const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
@@ -10,13 +8,11 @@ blogsRouter.get('/', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
-  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  const user = request.user
 
-  if (!decodedToken.id) {
+  if (!user) {
     return response.status(401).json({ error: 'token invalid'})
   }
-
-  const user = await User.findById(decodedToken.id)
 
   const blog = new Blog({
     title: body.title,
@@ -35,20 +31,21 @@ blogsRouter.post('/', async (request, response) => {
 
 blogsRouter.delete('/:id', async (request, response) => {
 
-  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  const user = request.user
 
-  if (!decodedToken.id) {
+  if (!user) {
     return response.status(401).json({ error: 'token invalid'})
   }
-  const blog = await Blog.findByIdAndDelete(request.params.id)
+  const blog = await Blog.findById(request.params.id)
 
   if (!blog) {
     return response.status(204).end()
   }
 
-  if (blog.user.toString() !== decodedToken.id) {
+  if (blog.user.toString() !== user.id.toString()) {
     return response.status(401).json({ error: 'only the creator can delete blogs' })
   }
+  
   await Blog.findByIdAndDelete(request.params.id)
   response.status(204).end()
 })
@@ -62,13 +59,5 @@ blogsRouter.put('/:id', async (request, response) => {
   const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
   response.json(updatedBlog)
 })
-
-const getTokenFrom = request => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.startsWith('Bearer ')) {
-    return authorization.replace('Bearer ', '')
-  }
-  return null
-}
 
 module.exports = blogsRouter
--- part4/utils/middleware.js
+++ part4/utils/middleware.js
@@ -1,4 +1,6 @@
 const logger = require('./logger')
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
 
 const tokenExtractor = (request, response, next) => {
   const authorization = request.get('authorization')
@@ -6,6 +8,16 @@ const tokenExtractor = (request, response, next) => {
     request.token = authorization.replace('Bearer ', '')
   } else {
     request.token = null
+  }
+  next()
+}
+
+const userExtractor = async (request, response, next) => {
+  if (request.token) {
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    if (decodedToken.id) {
+      request.user = await User.findById(decodedToken.id)
+    }
   }
   next()
 }
@@ -28,5 +40,6 @@ const errorHandler = (error, request, response, next) => {
 
 module.exports = {
   errorHandler,
-  tokenExtractor
+  tokenExtractor,
+  userExtractor
 }
